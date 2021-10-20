f6c20cd586382e8e3ab631e875d84399bdd2b1a4
duongvituan <duongvituan99@gmail.com>
2021-10-20 17:00:48 +0700

    Fix error don't add SplashPlayer

--- godot-awesome-splash/addons/awesome_splash/plugin.gd
+++ godot-awesome-splash/addons/awesome_splash/plugin.gd
@@ -3,8 +3,8 @@ extends EditorPlugin
 
 
 func _enter_tree():
-	add_custom_type("SplashPlayer", "res://addons/awesome_splash/core/BaseSplashPlayer.gd", preload("res://addons/awesome_splash/core/SplashPlayer.gd"), preload("res://addons/awesome_splash/assets/sp_icon.png"))
+	add_custom_type("SplashPlayer", "Node2D", preload("res://addons/awesome_splash/core/SplashPlayer.gd"), preload("res://addons/awesome_splash/assets/sp_icon.png"))
 
 
 func _exit_tree():
-	pass
+	remove_custom_type("SplashPlayer")

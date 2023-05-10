67ce528b4e2d5e644d6f582895c8fc2eacb8b26c
AdrKacz <61620013+AdrKacz@users.noreply.github.com>
2023-05-10 09:01:15 +0000

    feat: add touch screen to skip

--- godot-awesome-splash/addons/awesome_splash/core/SplashContainer.gd
+++ godot-awesome-splash/addons/awesome_splash/core/SplashContainer.gd
@@ -19,9 +19,9 @@ func _ready():
 # type skip = SKIP_ONE_SCREEN_WHEN_CLICKED or SKIP_ALL_SCREEN_WHEN_CLICKED
 # Todo: update condition skip splash screen: when you click, when you press key...
 func _skip_awe_splash_by_event(event) -> bool:
-	return event is InputEventMouseButton \
-			and event.pressed \
-			and event.button_index == 1
+	return event.is_pressed() \
+		and ((event is InputEventMouseButton and event.button_index == 1) \
+		or (event is InputEventScreenTouch))
 
 
 # Todo: move to other screen here:
